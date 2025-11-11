07e1541dfb9d19c3fe95ce07defec4801bb2be5e
David <Davcisneros2000@gmail.com>
2025-11-11 08:49:17 -0500

    Add JS for navigation menu / functionality

--- main.js
+++ main.js
@@ -16,3 +16,22 @@ toggleThemeBtn.addEventListener('click', () => {
             }
         }
 })
+
+//NAVIGATION BAR//
+let mainMenu = document.querySelector('.mainMenu');
+let closeMenu = document.querySelector('.closeMenu');
+let openMenu = document.querySelector('.openMenu');
+
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
+
+
+function show() {
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+
+function close() {
+    mainMenu.style.top = "-100%";
+}
+  
