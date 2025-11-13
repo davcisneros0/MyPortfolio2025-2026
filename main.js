026b173bf679c47e71bdec4eca17ff840935eeec
David <Davcisneros2000@gmail.com>
2025-11-13 08:24:28 -0500

    Add JS for localstorage for toggling theme button throughout site

--- main.js
+++ main.js
@@ -1,21 +1,50 @@
 const toggleThemeBtn = document.getElementById('toggle-theme-btn');
 
-toggleThemeBtn.addEventListener('click', () => {
+document.addEventListener('DOMContentLoaded', () => {
 
-    const lightModeTheme = document.querySelector('link[href="CSS/index-lightmode.css"]');
+    // apply theme throughout website
+    function applyTheme(theme) {
+        const lightModeTheme = document.querySelector('link[href="CSS/index-lightmode.css"]');
 
-    // check if CSS file is already loaded to prevent duplication
-        if (!lightModeTheme) {
-            const linkElement = document.createElement('link');
-            linkElement.rel = 'stylesheet';
-            linkElement.href = 'CSS/index-lightmode.css';
-            document.head.appendChild(linkElement);
+        if (theme === 'light') {
+            // IF LIGHT MODE IS NOT USED YET, ADD IT INSIDE HEAD TAG
+            if (!lightModeTheme) {
+                const linkElement = document.createElement('link');
+                linkElement.rel = 'stylesheet';
+                linkElement.href = 'CSS/index-lightmode.css';
+                document.head.appendChild(linkElement);         
+            }
         } else {
-            if (lightModeTheme) {
-                lightModeTheme.remove();
+            // IF LIGHT MODE IS  USED, REMOVE         
+            if (lightModeTheme) {   
+            lightModeTheme.remove();     
             }
         }
-})
+    }
+
+    // SAVE THEME TO LOCAL STORAGE
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+        applyTheme(savedTheme);
+    } else {
+        // DEFAULT
+        applyTheme('dark');
+    }
+
+
+    // ONCE THEME BUTTON IS CLICKED, ADD THEME TO LOCALSTORAGE
+    toggleThemeBtn.addEventListener('click', () => {
+        const currentTheme = localStorage.getItem('theme') || 'dark';
+        const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+
+        // SAVE NEW THEME TO LOCALSTORAGE
+        localStorage.setItem('theme', newTheme);
+
+        // APPLY
+        applyTheme(newTheme);
+    });
+});
+
 
 //NAVIGATION BAR//
 let mainMenu = document.querySelector('.mainMenu');
